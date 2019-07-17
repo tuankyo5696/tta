1dd548a7763ef56e272a9d32cface14436add5ab
hongphuc2197 <trananhtuan526201@gmail.com>
2019-07-17 12:03:44 +0700

    first commit

---
+++ pages/ourwork.js
@@ -0,0 +1,16 @@
+import React from "react"
+//import {Link} from "gatsby"
+import Layout from "../components/layout/layout";
+import Head from './../components/head'
+import OurWork from './../components/ourWork/ourWork';
+
+const OurWorkPage =()=>{
+  return(
+    <Layout>
+      <OurWork />
+    </Layout>
+  )
+}
+
+
+export default OurWorkPage
